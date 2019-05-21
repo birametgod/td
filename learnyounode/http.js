b932822750f8c7276366f4f45f72d6f9562f8695
Mathieu TUDISCO <oss@mathieutu.dev>
2019-05-21 13:39:06 +0200

    Update http.js

--- learnyounode/http.js
+++ learnyounode/http.js
@@ -1,6 +1,7 @@
 const http = require('http');
+const port = process.argv[2];
 
-http.get(process.argv[2], res => {
+http.get(port, res => {
   res.setEncoding('utf8');
   res.on('data', data => console.log(data));
   res.on('error', console.error);
